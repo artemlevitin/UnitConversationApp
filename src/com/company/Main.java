a7ec88c13e64e6420ce0e8e3f0118fdab90c71b9
artemlevitin <levitin@live.com>
2018-07-16 17:02:39 +0300

    replace to  List

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,25 +2,25 @@ package com.company;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 
-    static public byte[] readFile(String filePath) {
+    static public List<String> readFile(String filePath) {
         try {
-            byte[] buff = Files.readAllBytes(Paths.get(filePath));
-            return buff;
+            return Files.readAllLines(Paths.get(filePath));
+
         } catch (Exception exc) {
             System.out.println(exc.getMessage());
             return null;
         }
     }
 
-    static public void parser (byte [] input){
-
-     }
+    static public void parser (List<String> input) {
+    }
 
 }
